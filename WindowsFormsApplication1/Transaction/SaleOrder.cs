a35f54ed1b57d2e71d5a2e18afc1e7e5a78a4740
Seqkart Solutions <Seqkart Solutions@SEQKARTSOLUTION>
2020-10-19 16:43:39 +0530

    11

--- WindowsFormsApplication1/Transaction/SaleOrder.cs
+++ WindowsFormsApplication1/Transaction/SaleOrder.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using DevExpress.XtraEditors;
 
 namespace WindowsFormsApplication1.Transaction
 {
@@ -16,31 +8,6 @@ namespace WindowsFormsApplication1.Transaction
         public SaleOrder()
         {
             InitializeComponent();
-        }
-
-        private void labelControl11_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textEdit12_EditValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textEdit8_EditValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void labelControl9_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textEdit9_EditValueChanged(object sender, EventArgs e)
-        {
-
         }
     }
 }
