e77e88c0b8d46a882c8a944b5d2c410a6d3de2f6
Rohit Aggarwal <rohitaggarwal@creativelineindia.com>
2021-07-20 19:07:37 +0530

    wdef

--- WindowsFormsApplication1/Master/frmAccountMstAddEdit.cs
+++ WindowsFormsApplication1/Master/frmAccountMstAddEdit.cs
@@ -746,8 +746,7 @@ namespace WindowsFormsApplication1
 
                 TxnRespWithObjAndInfo<GSTINDetail> TxnResp = await EWBAPI.GetGSTNDetailAsync(EwbSession, txtGSTNo.Text);
 
-
-                if (TxnResp.IsSuccess)
+                                if (TxnResp.IsSuccess)
                 {
                     TextEdit t = new TextEdit();
                     t.Text = JsonConvert.SerializeObject(TxnResp.RespObj);
